3766a123af3c322cb88039b61cec4be66bc57ee3
renatodossantosoliveira <renato.soliveira81@gmail.com>
2024-06-03 23:47:45 -0300

    projeto base

---
+++ FileConverter/src/test/java/com/luizalabs/fileconverter/FileConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luizalabs.fileconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileConverterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
